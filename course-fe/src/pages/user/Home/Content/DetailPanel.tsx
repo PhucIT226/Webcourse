8cbc6b31a98145d3001791c7b11ef9568e2e9e3a
PhucIT226 <pdao01648@gmail.com>
2025-09-27 22:07:32 +0700

    complete homepage

---
+++ course-fe/src/pages/user/Home/Content/DetailPanel.tsx
@@ -0,0 +1,73 @@
+import { useState } from "react";
+import "../../../../styles/user/home/Detail.scss";
+import {
+  FaHandsHelping,
+  FaCertificate,
+  FaChartLine,
+  FaCogs,
+} from "react-icons/fa";
+
+const DetailPanel = () => {
+  const items = [
+    {
+      id: 1,
+      title: "Hands-on training",
+      desc: "Upskill effectively with AI-powered coding exercises, practice tests, and quizzes.",
+      icon: <FaHandsHelping />,
+    },
+    {
+      id: 2,
+      title: "Certification prep",
+      desc: "Prep for industry-recognized certifications by solving real-world challenges and earn badges along the way.",
+      icon: <FaCertificate />,
+    },
+    {
+      id: 3,
+      title: "Insights and analytics",
+      desc: "Fast-track goals with advanced insights plus a dedicated customer success team to help drive effective learning.",
+      icon: <FaChartLine />,
+    },
+    {
+      id: 4,
+      title: "Customizable content",
+      desc: "Create tailored learning paths for team and organization goals and even host your own content and resources.",
+      icon: <FaCogs />,
+    },
+  ];
+  const [active, setActive] = useState(null);
+
+  return (
+    <>
+      <div className="container">
+        <div className="title mb-5">
+          <h2>Learning focused on your goals</h2>
+        </div>
+        <div className="d-flex justify-content-between">
+          <div className="sidebar d-flex flex-column gap-4">
+            {items.map((item) => (
+              <button
+                key={item.id}
+                className={`menu-btn ${active === item.id ? "active" : ""}`}
+                onClick={() => setActive(item.id)}
+              >
+                <span className="icon">{item.icon}</span>
+                {item.title}
+              </button>
+            ))}
+          </div>
+          <div className="content">
+            {active ? (
+              <div className="info">
+                <h2>{items.find((i) => i.id === active)?.title}</h2>
+                <p>{items.find((i) => i.id === active)?.desc}</p>
+              </div>
+            ) : (
+              <p>👉 Bấm vào menu bên trái để xem chi tiết</p>
+            )}
+          </div>
+        </div>
+      </div>
+    </>
+  );
+};
+export default DetailPanel;
--- course-fe/src/pages/user/Home/Home.tsx
+++ course-fe/src/pages/user/Home/Home.tsx
@@ -1,63 +1,32 @@
-import { Swiper, SwiperSlide } from "swiper/react";
-import { Autoplay, Pagination, Navigation } from "swiper/modules";
-import "swiper/css";
-import "swiper/css/pagination";
-import "swiper/css/navigation";
-
 import "../../../styles/user/home/home.scss";
 import Courses from "./Content/Coures";
+import Categories from "./Content/Categories";
+import Slider from "./Content/Slider";
+import "../../../styles/user/home/home.scss";
+import DetailPanel from "./Content/DetailPanel";
+import Testimonials from "./Content/Testimonials";
+import Footer from "./Footer/Footer";
 
 const Home = () => {
   return (
     <>
-      <div className="swiper-container mb-3">
-        <Swiper
-          spaceBetween={30}
-          autoplay={{
-            delay: 3000, // đổi slide mỗi 3s
-            disableOnInteraction: false,
-          }}
-          pagination={{ clickable: true }}
-          navigation={true}
-          modules={[Autoplay, Pagination, Navigation]}
-          className="hero-swiper"
-        >
-          <SwiperSlide>
-            <div className="slide-content">
-              <div className="text-box">
-                <h2>Học ReactJS Miễn Phí!</h2>
-                <p>
-                  Khóa học ReactJS từ cơ bản tới nâng cao. Kết quả của khóa học
-                  này là bạn có thể làm hầu hết các dự án thường gặp với
-                  ReactJS.
-                </p>
-                <button className="btn btn-info">Đăng ký ngay</button>
-              </div>
-              <div className="img-box">
-                <img src="../src/assets/react.png" alt="ReactJS" />
-              </div>
-            </div>
-          </SwiperSlide>
-
-          <SwiperSlide>
-            <div className="slide-content-2">
-              <div className="text-box">
-                <h2>NodeJS Backend</h2>
-                <p>
-                  Học cách xây dựng REST API với Express, Sequelize, và JWT
-                  Authentication.
-                </p>
-                <button className="btn btn-primary">Học ngay</button>
-              </div>
-              <div className="img-box">
-                <img src="../src/assets/nodejs.png" alt="NodeJS" />
-              </div>
-            </div>
-          </SwiperSlide>
-        </Swiper>
+      <div className="swiper-container mb-5">
+        <Slider />
       </div>
-      <div className="courses-container">
+      <div className="courses-container mb-5">
         <Courses />
+      </div>
+      <div className="categories-container mb-5">
+        <Categories />
+      </div>
+      <div className="detail-container">
+        <DetailPanel />
+      </div>
+      <div className="testimonial-container">
+        <Testimonials />
+      </div>
+      <div className="footer-container">
+        <Footer />
       </div>
     </>
   );
